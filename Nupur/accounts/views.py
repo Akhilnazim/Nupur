b90a2b4a04e1e0954248b96465327aa99b8e960c
Akhil Nazim <anunazim1@gmail.com>
2020-05-06 07:41:49 +0530

    second commit

--- Nupur/accounts/views.py
+++ Nupur/accounts/views.py
@@ -1,5 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.models import User, auth
 
 # Create your views here.
-def signup(request):
-    return render(request,'my-account.html')
+
+
+def login(request):
+    if request.method == 'POST':
+         username = request.POST['username']
+         password = request.POST['password']
+
+         user = auth.authenticate(username=username, password=password)
+
+         if user is not None:
+             auth.login(request, user)
+             return redirect('/')
+         else:
+            return redirect('login')
+    else:
+         return render(request, 'my-account.html')
+def register(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        if User.objects.filter(username=username).exists():
+            # messages.info(request, 'Username taken')
+            return redirect('register')
+        else:
+            user = User.objects.create_user(username=username, password=password)
+            user.save();
+            print("user created")
+            return redirect('/')
+            # return redirect("index.html")
+        return redirect('/')
+
+    else:
+        return render(request, 'my-account.html')
--- Nupur/storola/views.py
+++ Nupur/storola/views.py
@@ -5,5 +5,5 @@ from django.http import HttpResponse
 def index(request):
     return render(request,'index.html')
 
-def signup(request):
-    return render(request,'my-account.html')
+# def signup(request):
+#     return render(request,'my-account.html')
